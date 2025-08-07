38cb37ffb6bef8f03128252d9dfdf65df4262684
Mikallina <mi_borges@msn.coma>
2025-08-07 14:55:06 -0300

    Setup do projeto

---
+++ Board_de_tarefas/BoardDeTarefas/src/main/java/br/com/santander/BoardDeTarefas/BoardDeTarefasApplication.java
@@ -0,0 +1,13 @@
+package br.com.santander.BoardDeTarefas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardDeTarefasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoardDeTarefasApplication.class, args);
+	}
+
+}
---
+++ Board_de_tarefas/BoardDeTarefas/src/test/java/br/com/santander/BoardDeTarefas/BoardDeTarefasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.santander.BoardDeTarefas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardDeTarefasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
